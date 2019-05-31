f653561666bbba2ce4107539554975aef4cdff6b
senseyvik22 <senseyvik22@gmail.com>
2019-05-31 17:24:15 +0300

    untracked files

---
+++ ComputerAccessoriesStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,76 @@
+﻿using ComputerAccessoriesStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputerAccessoriesStore.Domain.Entities;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace ComputerAccessoriesStore.Domain.Concrete
+{
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings emailSettings)
+        {
+            this.emailSettings = emailSettings;
+        }
+        //232 str
+        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+                
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("A new order has been submitted")
+                    .AppendLine("---")
+                    .AppendLine("Items:");
+
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Product.Price * line.Quantity;
+
+                    body.AppendLine($"{line.Quantity} x {line.Product.Name} (subtotal: {subtotal.ToString("c")}");
+                }
+
+                //else some info
+
+
+                MailMessage msg = new MailMessage(
+                    emailSettings.MailFromAddress,
+                    emailSettings.MailToAddress,
+                    "New order submitted",
+                    body.ToString()
+                );
+
+                if (emailSettings.WriteAsFile)
+                {
+                    msg.BodyEncoding = Encoding.ASCII;
+                    File.WriteAllText(emailSettings.FileLocation, msg.ToString());
+                }
+                else
+                {
+                    smtpClient.Send(msg);
+                }
+            }
+
+
+        }
+    }
+}
