6f29dd0de4900aeaefe33e7eaed7f33e6376754c
senseyvik22 <senseyvik22@gmail.com>
2019-05-31 17:19:52 +0300

    done delivery

--- ComputerAccessoriesStore.WebUI/Controllers/CartController.cs
+++ ComputerAccessoriesStore.WebUI/Controllers/CartController.cs
@@ -12,10 +12,12 @@ namespace ComputerAccessoriesStore.WebUI.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductRepository rep)
+        public CartController(IProductRepository rep, IOrderProcessor proc)
         {
             repository = rep;
+            orderProcessor = proc;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -56,6 +58,31 @@ namespace ComputerAccessoriesStore.WebUI.Controllers
         public PartialViewResult Summary(Cart cart)
         {
             return PartialView(cart);
+        }
+
+        [HttpPost]
+        public ViewResult Checkout (Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
         }
 
         //private Cart GetCart()
